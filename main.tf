e3fe20f9165b373dfbb5810f5985c51871b25449
Saksham Awasthi <74664860+Cipher-08@users.noreply.github.com>
2024-10-25 13:21:50 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ resource "aws_security_group" "insecure_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  # This allows unrestricted SSH access
+    cidr_blocks = ["0.0.0.0/0"]  # Unrestricted access
   }
 
   egress {
